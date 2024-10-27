0613cc30bdb601fa0cead81de4c4818e5ce48498
GabrielGC <goncalves.cunhaa@gmail.com>
2024-10-27 15:37:39 -0300

    feat: adicionar lógica de slide na galeria de produtos

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-var radio = document.querySelector('.manual__btn')
-var cont = 1
+// Hero
+var radio = document.querySelector(".manual__btn");
+var cont = 1;
 
-document.getElementById('radio1').checked = true
+document.getElementById("radio1").checked = true;
 
 setInterval(() => {
-    proximaImg()
-}, 7000)
+  proximaImg();
+}, 5000);
 
-function proximaImg(){
-    cont++
+function proximaImg() {
+  cont++;
 
-    if(cont > 3){
-        cont = 1 
-    }
+  if (cont > 3) {
+    cont = 1;
+  }
 
-    document.getElementById('radio'+cont).checked = true
+  document.getElementById("radio" + cont).checked = true;
 }
+
+// Galeria de produtos
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var button = document.getElementById("slide__right");
+    button.onclick = function () {
+      document.getElementById("roww").scrollLeft += 300;
+    };
+    var button = document.getElementById("slide__left");
+    button.onclick = function () {
+      document.getElementById("roww").scrollLeft -= 300;
+    };
+  },
+  false
+);
