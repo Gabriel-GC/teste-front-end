265aa0d05e8f216249d5b421e06733e358897bd5
GabrielGC <goncalves.cunhaa@gmail.com>
2024-10-29 14:41:23 -0300

    feat: adiciona lógica de menu retrátil footer mobile

--- app.js
+++ app.js
@@ -147,3 +147,21 @@ document
 function mensageSubmit() {
   alert("Newslatter assinada");
 }
+
+
+// Footer mobile
+var acc = document.getElementsByClassName("footer__mobile__panel");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
